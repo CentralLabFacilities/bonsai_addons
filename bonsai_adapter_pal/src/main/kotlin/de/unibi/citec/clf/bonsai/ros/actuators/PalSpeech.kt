bbccc74190d3b57ef1de625629f3edcfd879a668
Leroy <lruegeme@techfak.uni-bielefeld.de>
2018-11-11 23:04:20 +0100

    pal: add ps disable

--- bonsai_adapter_pal/src/main/kotlin/de/unibi/citec/clf/bonsai/ros/actuators/PalSpeech.kt
+++ bonsai_adapter_pal/src/main/kotlin/de/unibi/citec/clf/bonsai/ros/actuators/PalSpeech.kt
@@ -6,16 +6,20 @@ import de.unibi.citec.clf.bonsai.actuators.SpeechActuator
 import de.unibi.citec.clf.bonsai.core.configuration.IObjectConfigurator
 import de.unibi.citec.clf.bonsai.core.exception.ConfigurationException
 import de.unibi.citec.clf.bonsai.ros.RosNode
+import de.unibi.citec.clf.bonsai.ros.helper.ResponseFuture
 import org.ros.exception.RosException
 import org.ros.message.Duration
 import org.ros.namespace.GraphName
 import org.ros.node.ConnectedNode
+import org.ros.node.service.ServiceClient
 import java.util.concurrent.Future
 
 import pal_interaction_msgs.TtsActionFeedback
 import pal_interaction_msgs.TtsActionGoal
 import pal_interaction_msgs.TtsActionResult
+import std_srvs.*
 import java.io.IOException
+import java.util.concurrent.TimeUnit
 
 /**
  *
@@ -25,7 +29,9 @@ class PalSpeech(private val nodeName: GraphName) : RosNode(), SpeechActuator {
 
     private val logger = org.apache.log4j.Logger.getLogger(javaClass)
     private var ac: ActionClient<TtsActionGoal, TtsActionFeedback, TtsActionResult>? = null
+    private var clientDisableSpeech: ServiceClient<SetBoolRequest, SetBoolResponse>? = null
     private lateinit var topic: String
+    private var disableSpeechTopic: String? = null
 
     init {
         initialized = false
@@ -38,11 +44,14 @@ class PalSpeech(private val nodeName: GraphName) : RosNode(), SpeechActuator {
     @Throws(ConfigurationException::class)
     override fun configure(conf: IObjectConfigurator) {
         topic = conf.requestValue("topic")
+        disableSpeechTopic = conf.requestOptionalValue("disableSpeechTopic", disableSpeechTopic)
     }
 
     override fun onStart(connectedNode: ConnectedNode) {
         ac = ActionClient(connectedNode, this.topic, TtsActionGoal._TYPE, TtsActionFeedback._TYPE, TtsActionResult._TYPE)
-
+        disableSpeechTopic?.let {
+            clientDisableSpeech = connectedNode.newServiceClient(it,SetBool._TYPE)
+        }
         if(ac?.waitForActionServerToStart(Duration(2.0)) ==  true) {
             logger.info("PalSpeech server connected $topic")
             initialized = true
@@ -75,14 +84,58 @@ class PalSpeech(private val nodeName: GraphName) : RosNode(), SpeechActuator {
 
     }
 
+    private fun enableSpeech(enabled: Boolean) {
+        clientDisableSpeech?.let {
+            val a = it.newMessage()
+            a.data = enabled
+            val res = ResponseFuture<SetBoolResponse>()
+            it.call(a, res)
+        } ?: logger.warn("SpeechRec not connected")
+
+    }
+
     @Throws(IOException::class)
     override fun say(text: String) {
+        enableSpeech(false)
         sendToTTS(text).get()
+        enableSpeech(true)
     }
 
     @Throws(IOException::class)
     override fun sayAsync(text: String): Future<Void> {
-        return sendToTTS(text).toVoidFuture()
+        enableSpeech(false)
+        val ret = sendToTTS(text)
+        return object : Future<Void> {
+            override fun isDone(): Boolean {
+                val done = ret.isDone()
+                if(done) enableSpeech(true)
+                return done
+            }
+
+            override fun get(): Void {
+                val a = ret.toVoidFuture().get()
+                enableSpeech(true)
+                return a
+            }
+
+            override fun get(p0: Long, p1: TimeUnit?): Void {
+                val a = ret.toVoidFuture().get(p0,p1)
+                enableSpeech(true)
+                return a
+            }
+
+            override fun cancel(p0: Boolean): Boolean {
+                val b = ret.cancel(p0)
+                enableSpeech(true)
+                return b
+            }
+
+            override fun isCancelled(): Boolean {
+                return ret.isCancelled()
+            }
+
+        }
+
     }
 
     override fun sayAccentuated(accented_text: String?) {
