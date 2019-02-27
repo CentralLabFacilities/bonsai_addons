22611964efbbd98bed98590d4fe38ea936621030
Leroy Rügemer <lruegeme@techfak.uni-bielefeld.de>
2019-02-27 09:14:55 +0100

    ros_clf: clf_grasping_msgs version bump

--- bonsai_adapter_ros_clf/src/main/java/de/unibi/citec/clf/bonsai/ros/actuators/ClfGraspingPlanningSceneActuator.java
+++ bonsai_adapter_ros_clf/src/main/java/de/unibi/citec/clf/bonsai/ros/actuators/ClfGraspingPlanningSceneActuator.java
@@ -1,27 +1,20 @@
 
 package de.unibi.citec.clf.bonsai.ros.actuators;
 
-import actionlib_msgs.GoalStatusArray;
-import std_srvs.Empty;
-import std_srvs.EmptyRequest;
-import std_srvs.EmptyResponse;
-import com.github.rosjava_actionlib.ActionClient;
 import de.unibi.citec.clf.bonsai.actuators.PlanningSceneActuator;
 import de.unibi.citec.clf.bonsai.core.configuration.IObjectConfigurator;
 import de.unibi.citec.clf.bonsai.core.exception.ConfigurationException;
 import de.unibi.citec.clf.bonsai.ros.RosNode;
 import de.unibi.citec.clf.bonsai.ros.helper.ResponseFuture;
 import de.unibi.citec.clf.btl.data.object.ObjectShapeList;
-import knowledge_base_msgs.QueryResponse;
+import org.apache.commons.lang.NotImplementedException;
 import org.ros.exception.ServiceNotFoundException;
-import org.ros.message.Duration;
 import org.ros.namespace.GraphName;
 import org.ros.node.ConnectedNode;
 import org.ros.node.service.ServiceClient;
-import planning_scene_manager_msgs.PlanningSceneManagerRequestActionFeedback;
-import planning_scene_manager_msgs.PlanningSceneManagerRequestActionGoal;
-import planning_scene_manager_msgs.PlanningSceneManagerRequestActionResult;
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+import std_srvs.Empty;
+import std_srvs.EmptyRequest;
+import std_srvs.EmptyResponse;
 
 import java.util.concurrent.Future;
 
