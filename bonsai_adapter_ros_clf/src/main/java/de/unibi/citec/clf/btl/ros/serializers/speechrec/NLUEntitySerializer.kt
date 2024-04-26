6ab4b23805c9575c921ffd552b21e1b2f9e06b4c
lruegeme <lruegeme@techfak.uni-bielefeld.de>
2024-04-26 15:42:00 +0200

    add clf_speech serializer

---
+++ bonsai_adapter_ros_clf/src/main/java/de/unibi/citec/clf/btl/ros/serializers/speechrec/NLUEntitySerializer.kt
@@ -0,0 +1,38 @@
+package de.unibi.citec.clf.btl.ros.serializers.speechrec
+
+import de.unibi.citec.clf.btl.data.speechrec.NLUEntity
+import de.unibi.citec.clf.btl.ros.RosSerializer
+import org.apache.log4j.Logger
+import org.ros.message.MessageFactory
+
+/**
+ *
+ * @author lruegeme
+ */
+class NLUEntitySerializer : RosSerializer<NLUEntity, clf_speech_msgs.Entity>() {
+    override fun getDataType(): Class<NLUEntity> {
+        return NLUEntity::class.java
+    }
+
+    override fun getMessageType(): Class<clf_speech_msgs.Entity> {
+        return clf_speech_msgs.Entity::class.java
+    }
+
+    @Throws(DeserializationException::class)
+    override fun deserialize(msg: clf_speech_msgs.Entity): NLUEntity {
+        return NLUEntity(msg.key, msg.value, msg.role)
+    }
+
+    @Throws(SerializationException::class)
+    override fun serialize(data: NLUEntity, fact: MessageFactory): clf_speech_msgs.Entity {
+        val e : clf_speech_msgs.Entity = fact.newFromType(clf_speech_msgs.Entity._TYPE)
+        e.value = data.entity
+        e.key = data.key
+        e.role = data.role
+        return e
+    }
+
+    companion object {
+        private val logger = Logger.getLogger(NLUEntitySerializer::class.java)
+    }
+}
---
+++ bonsai_adapter_ros_clf/src/main/java/de/unibi/citec/clf/btl/ros/serializers/speechrec/NLUSerializer.kt
@@ -0,0 +1,54 @@
+package de.unibi.citec.clf.btl.ros.serializers.speechrec
+
+import de.unibi.citec.clf.btl.data.speechrec.NLU
+import de.unibi.citec.clf.btl.data.speechrec.NLUEntity
+import de.unibi.citec.clf.btl.ros.MsgTypeFactory
+import de.unibi.citec.clf.btl.ros.RosSerializer
+import org.apache.log4j.Logger
+import org.ros.message.MessageFactory
+
+/**
+ *
+ * @author lruegeme
+ */
+class NLUSerializer : RosSerializer<NLU, clf_speech_msgs.NLU>() {
+    override fun getDataType(): Class<NLU> {
+        return NLU::class.java
+    }
+
+    override fun getMessageType(): Class<clf_speech_msgs.NLU> {
+        return clf_speech_msgs.NLU::class.java
+    }
+
+    @Throws(DeserializationException::class)
+    override fun deserialize(msg: clf_speech_msgs.NLU): NLU {
+
+        val nluentities = msg.entities.stream().map {
+            MsgTypeFactory.getInstance().createType(it, NLUEntity::class.java)
+        }.toList()
+
+        return NLU(msg.text, msg.intent, msg.conf, nluentities)
+    }
+
+    @Throws(SerializationException::class)
+    override fun serialize(data: NLU, fact: MessageFactory): clf_speech_msgs.NLU {
+        val msg = fact.newFromType<clf_speech_msgs.NLU>(clf_speech_msgs.NLU._TYPE)
+
+        return msg.apply {
+            text = data.text
+            intent = data.intent
+            conf = data.confidence
+            entities.addAll(data.map {
+                val emsg : clf_speech_msgs.Entity = fact.newFromType(clf_speech_msgs.Entity._TYPE)
+                emsg.key = it?.key
+                emsg.value = it?.entity
+                emsg.role = it?.role
+                emsg
+            })
+        }
+    }
+
+    companion object {
+        private val logger = Logger.getLogger(NLUSerializer::class.java)
+    }
+}
