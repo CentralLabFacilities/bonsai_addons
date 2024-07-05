641ec96273023f966425599f175182b6b99c2e56
lruegeme <lruegeme@techfak.uni-bielefeld.de>
2024-07-05 12:06:23 +0200

    pal: IsSomethingInGripper now defaults to using both sensors

--- bonsai_adapter_pal/src/main/kotlin/de/unibi/citec/clf/bonsai/skills/pal/IsSomethingInGripper.kt
+++ bonsai_adapter_pal/src/main/kotlin/de/unibi/citec/clf/bonsai/skills/pal/IsSomethingInGripper.kt
@@ -7,36 +7,39 @@ import de.unibi.citec.clf.bonsai.engine.model.ExitStatus
 import de.unibi.citec.clf.bonsai.engine.model.ExitToken
 import de.unibi.citec.clf.bonsai.engine.model.config.ISkillConfigurator
 import de.unibi.citec.clf.btl.data.speechrec.Utterance
+import net.sf.saxon.functions.ConstantFunction.True
 import std_msgs.Bool
 
-class IsSomethingInGripper : AbstractSkill(), SensorListener<Boolean> {
+class IsSomethingInGripper : AbstractSkill() {
+    private var use_both = true
 
     private lateinit var tokenSuccessYes: ExitToken
     private lateinit var tokenSuccessNo: ExitToken
     private var gripperSensor: Sensor<Boolean>? = null
-    private var full = false
+    private var gripperSensorOther: Sensor<Boolean>? = null
 
     override fun configure(configurator: ISkillConfigurator) {
         tokenSuccessYes = configurator.requestExitToken(ExitStatus.SUCCESS().ps("yes"))
         tokenSuccessNo = configurator.requestExitToken(ExitStatus.SUCCESS().ps("no"))
+        use_both = configurator.requestOptionalBool("use_both", use_both)
         gripperSensor = configurator.getSensor("IsSomethingInGripper", Boolean::class.java)
+        if (use_both) gripperSensorOther = configurator.getSensor("IsSomethingInGripper2", Boolean::class.java)
     }
 
     override fun init(): Boolean {
-        gripperSensor?.addSensorListener(this)
         return true
     }
 
     override fun execute(): ExitToken {
-        var full : Boolean = gripperSensor?.readLast(0)?: return ExitToken.fatal()
-        return if (full) tokenSuccessYes else tokenSuccessNo
+        val full : Boolean = gripperSensor?.readLast(0)?: return ExitToken.fatal()
+        val fullOther : Boolean = gripperSensorOther?.readLast(0)?: if (use_both) return ExitToken.fatal() else false
+
+        return if (full || fullOther) tokenSuccessYes else tokenSuccessNo
     }
 
     override fun end(curToken: ExitToken): ExitToken {
         return  curToken
     }
 
-    override fun newDataAvailable(newData: Boolean) {
-        if(newData) full = true
-    }
+
 }
